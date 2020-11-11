ec61d93c4020e16d4e3f0e50be454a09ff9b408e
hanearkim226@gmail.com <rlagksdjf1!>
2020-11-11 11:19:04 +0900

    Dataloader의 tokenizer 방식 변경

--- dataloader.py
+++ dataloader.py
@@ -6,18 +6,16 @@ import logging
 import torch
 from torch.utils import data
 from torch.utils.data import TensorDataset
-from torch.utils.data import Dataset
 
-import re
-import emoji
-from soynlp.normalizer import repeat_normalize
-
-from transformers import AutoTokenizer
-
-from config import Config
-from new_dataloader import MyDataset
+from torchvision import datasets, transforms
+from PIL import Image
+import pandas as pd
+from transformers import AutoTokenizer, AutoModel, AutoConfig
 
 logger = logging.getLogger(__name__)
+
+BERT_MODEL_NAME = 'beomi/kcbert-base'
+MAX_SEQ_LEN = 100
 
 
 class InputExample(object):
@@ -89,41 +87,17 @@ class NsmcProcessor(object):
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
-        emojis = ''.join(emoji.UNICODE_EMOJI.keys())
-        p0 = re.compile(f'[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-힣{emojis}]+')
-        p1 = re.compile(r'(\"){2,}')
-        p2 = re.compile(r'(\?){2,}')
-        p3 = re.compile(r'(\!){2,}')
-        p4 = re.compile(r'(\.){2,}')
-        p5 = re.compile(r'(\~){2,}')
-        p6 = re.compile(r'(\;){2,}')
-        p7 = re.compile(r'(\^){2,}')
-        p8 = re.compile(r'(\*){2,}')
-        p9 = re.compile(
-            r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
-
-        pattern = [(' ', p0), ("", p1), ("?", p2), ("!", p3), ("...", p4), ("~", p5), (";", p6), ("^^", p7), ("*", p8), (" ", p9)]
-
-        def clean(x):
-            for ch, sub_pattern in pattern:
-                x = sub_pattern.sub(ch, x)
-            x = x.strip()
-            x = repeat_normalize(x, num_repeats=2)
-            return x
-
         examples = []
-        # for (i, line) in (lines[1:]):
         for i in range(1, len(lines)):
             line = lines[i].split('\t')
             guid = "%s-%s" % (set_type, i)
-            try:
-                text_a = clean(line[0])
-                label = int(line[1])
-            except:
-                print('[exept] ', line)
+            if len(line) != 2:
                 continue
-            # if i % 1000 == 0:
-            #     logger.info(lines[i])
+
+            text_a = line[0]
+            label = int(line[1])
+            if i % 1000 == 0:
+                logger.info(lines[i])
             examples.append(InputExample(guid=guid, text_a=text_a, label=label))
         return examples
 
@@ -149,52 +123,23 @@ processors = {
 }
 
 
-def convert_examples_to_features(examples, max_seq_len, tokenizer,
-                                 cls_token_segment_id=0,
-                                 pad_token_segment_id=0,
-                                 sequence_a_segment_id=0,
-                                 mask_padding_with_zero=True):
-    # Setting based on the current model type
-    cls_token = tokenizer.cls_token
-    sep_token = tokenizer.sep_token
-    pad_token_id = tokenizer.pad_token_id
-
+def convert_examples_to_features(examples, max_seq_len, tokenizer):
     features = []
     for (ex_index, example) in enumerate(examples):
         if ex_index % 5000 == 0:
             logger.info("Writing example %d of %d" % (ex_index, len(examples)))
 
-        tokens = tokenizer(example.text_a, padding='max_length', truncation=True, max_length=max_seq_len, return_tensors="pt")
+        tokens = tokenizer(example.text_a, padding='max_length', truncation=True, return_tensors='pt', max_length=max_seq_len)
 
-#         # Account for [CLS] and [SEP]
-#         special_tokens_count = 2
-#         if len(tokens) > max_seq_len - special_tokens_count:
-#             tokens = tokens[:(max_seq_len - special_tokens_count)]
+        input_ids = tokens['input_ids'][0].tolist()
+        attention_mask = tokens['attention_mask'][0].tolist()
+        token_type_ids = tokens['token_type_ids'][0].tolist()
 
-#         # Add [SEP] token
-#         tokens += [sep_token]
-#         token_type_ids = [sequence_a_segment_id] * len(tokens)
-
-#         # Add [CLS] token
-#         tokens = [cls_token] + tokens
-#         token_type_ids = [cls_token_segment_id] + token_type_ids
-
-#         input_ids = tokenizer.convert_tokens_to_ids(tokens)
-
-#         # The mask has 1 for real tokens and 0 for padding tokens. Only real
-#         # tokens are attended to.
-#         attention_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
-
-        # Zero-pad up to the sequence length.
-        input_ids = tokens['input_ids']
-        attention_mask = tokens['attention_mask']
-        token_type_ids = tokens['token_type_ids']
-
-        # assert len(input_ids) == max_seq_len, "Error with input length {} vs {}".format(len(input_ids), max_seq_len)
-        # assert len(attention_mask) == max_seq_len, "Error with attention mask length {} vs {}".format(
-        #     len(attention_mask), max_seq_len)
-        # assert len(token_type_ids) == max_seq_len, "Error with token type length {} vs {}".format(len(token_type_ids),
-        #                                                                                           max_seq_len)
+        assert len(input_ids) == max_seq_len, "Error with input length {} vs {}".format(len(input_ids), max_seq_len)
+        assert len(attention_mask) == max_seq_len, "Error with attention mask length {} vs {}".format(
+            len(attention_mask), max_seq_len)
+        assert len(token_type_ids) == max_seq_len, "Error with token type length {} vs {}".format(len(token_type_ids),
+                                                                                                  max_seq_len)
 
         label_id = example.label
 
@@ -214,6 +159,8 @@ def convert_examples_to_features(examples, max_seq_len, tokenizer,
                           label_id=label_id
                           ))
 
+        break
+
     return features
 
 
@@ -222,14 +169,14 @@ def load_and_cache_examples(root, tokenizer, mode):
 
     # Load data features from cache or dataset file
     cached_file_name = 'cached_{}_{}_{}_{}'.format(
-        'nsmc', list(filter(None, Config.BERT_MODEL_NAME.split("/"))).pop(), 300, mode)
+        'nsmc', list(filter(None, BERT_MODEL_NAME.split("/"))).pop(), MAX_SEQ_LEN, mode)
 
-    cached_features_file = os.path.join(Config.TRAIN_DIR, cached_file_name)
+    cached_features_file = os.path.join(root, cached_file_name)
     if os.path.exists(cached_features_file):
         logger.info("Loading features from cached file %s", cached_features_file)
         features = torch.load(cached_features_file)
     else:
-        logger.info("Creating features from dataset file at %s", Config.TRAIN_DIR)
+        logger.info("Creating features from dataset file at %s", root)
         if mode == "train":
             examples = processor.get_examples("train")
         elif mode == "validate":
@@ -239,7 +186,7 @@ def load_and_cache_examples(root, tokenizer, mode):
         else:
             raise Exception("For mode, Only train, dev, test is available")
 
-        features = convert_examples_to_features(examples, 100, tokenizer)
+        features = convert_examples_to_features(examples, MAX_SEQ_LEN, tokenizer)
         logger.info("Saving features into cached file %s", cached_features_file)
         torch.save(features, cached_features_file)
 
@@ -255,7 +202,7 @@ def load_and_cache_examples(root, tokenizer, mode):
 
 
 def data_loader(root, phase, batch_size, tokenizer):
-    dataset = MyDataset(phase)
+    dataset = load_and_cache_examples(root, tokenizer, mode=phase)
 
     if phase == 'train':
         sampler = data.RandomSampler(dataset)
