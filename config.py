1855e450e360b5b2d13b77c2b6391d0ae5c2e420
hanearkim226@gmail.com <rlagksdjf1!>
2020-11-13 14:52:17 +0900

    focal loss 파라미터 추가

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import json
 
 
 class Config:
-    def __init__(self, config_path):
+    def __init__(self, config_path='config.json'):
         with open(config_path) as f:
             config = json.load(f)
         self.__dict__ = config
